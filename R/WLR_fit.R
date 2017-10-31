fb24d50b308a6a2cde57f9b8b61456f193b1fb4d
Quresh Latif <qureshlatif@yahoo.com>
2017-10-31 15:59:52 -0600

    add option to WLR_fit and WLR_AICtable
    add option for inserting custom weights

--- R/WLR_fit.R
+++ R/WLR_fit.R
@@ -1,8 +1,12 @@
 WLR_fit <-
-function(dat, mod, Obs = "Nest") { # Fit weighted logistic regression
+function(dat, mod, Obs = "Nest", w = NULL) { # Fit weighted logistic regression
   form <- paste0(Obs, "~", mod)
-  w <- rep(1, nrow(dat))
-  w[which(dat[, Obs] == 0)] <- sum(dat[, Obs] == 1) / sum(dat[, Obs] == 0)
+  if(is.null(w)) {
+    w <- rep(1, nrow(dat))
+    w[which(dat[, Obs] == 0)] <- sum(dat[, Obs] == 1) / sum(dat[, Obs] == 0)
+  } else {
+    w <- w
+  }
   dat$w <- w
   mod <- glm(form, family = binomial, weights = w, data = dat)
   return(mod)
