e2cbb717c830045dadedaec60fd5c4546a5db2d5
Quresh Latif <qureshlatif@yahoo.com>
2017-10-31 10:19:07 -0600

    minor correction to WLR_fit & WLR_AICtable
    Changes so only character string with right-hand side of model formula is required for the second argument of WLR_fit.

--- R/WLR_fit.R
+++ R/WLR_fit.R
@@ -1,8 +1,9 @@
 WLR_fit <-
-function(dat, formula, Obs = "Nest") { # Fit weighted logistic regression
+function(dat, mod, Obs = "Nest") { # Fit weighted logistic regression
+  form <- paste0(Obs, "~", mod)
   w <- rep(1, nrow(dat))
   w[which(dat[, Obs] == 0)] <- sum(dat[, Obs] == 1) / sum(dat[, Obs] == 0)
   dat$w <- w
-  mod <- glm(formula, family = binomial, weights = w, data = dat)
+  mod <- glm(form, family = binomial, weights = w, data = dat)
   return(mod)
 }
