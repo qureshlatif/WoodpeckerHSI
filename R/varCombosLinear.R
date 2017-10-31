9a550d509fc0d26fb7d862732fe465d06e2277a7
Quresh Latif <qureshlatif@yahoo.com>
2017-10-31 09:28:06 -0600

    debug varCombosLinear

--- R/varCombosLinear.R
+++ R/varCombosLinear.R
@@ -7,7 +7,7 @@ varCombosLinear <- function(V, K.max = length(vars), exclude = NULL) {
       m <- paste(v, collapse="+")
       if(!is.null(exclude)) {
         check <- 0
-        for(x in 1:length(exclude)) if(sum(exclude[[x]]) %in% v) check <- 1
+        for(x in 1:length(exclude)) if(sum(exclude[[x]] %in% v)>1) check <- 1
         if(check == 0) mods[[(length(mods)+1)]] <- m
       }
     }
