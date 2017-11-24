cedaa5326975cf9639b31c32525e09367d3aef42
Quresh Latif <qureshlatif@yahoo.com>
2017-11-24 14:54:40 -0700

    adjust density and plotting functions for flexible number of suitability classes
    Now allows any number of suitability classes. To introduce this flexibility, labeling of suitability classes and thresholds are removed from the plotting function and need to be added after running the function.

--- R/calcClassDensities.R
+++ R/calcClassDensities.R
@@ -1,9 +1,25 @@
 calcClassDensities <- function(sampleHSI, bgroundHSI, thresholds, area) {
-  dat.class <- data.frame(HSI.md = c(mean(bgroundHSI[which(bgroundHSI < thresholds[1])]),
-                                     mean(bgroundHSI[which(bgroundHSI >= thresholds[1] & bgroundHSI < thresholds[2])]),
-                                     mean(bgroundHSI[which(bgroundHSI >= thresholds[2])])),
-                          HSI.st = c(0, thresholds[1], thresholds[2]),
-                          HSI.end = c(thresholds[1], thresholds[2], 1))
+  if(nrow(dat.class) != (length(thresholds) - 1)) stop("Incorrect number of thresholds provided for the number of suitability classes.")
+  HSI.md <- HSI.st <- HSI.end <- numeric(length = (length(thresholds) + 1))
+  dat.class <- data.frame(HSI.md, HSI.st, HSI.end)
+  for(r in 1:length(HSI.md)) {
+    if(r == 1) {
+      dat.class$HSI.md[r] <- mean(bgroundHSI[which(bgroundHSI < thresholds[1])])
+      dat.class$HSI.st[r] <- 0
+      dat.class$HSI.end[r] <- thresholds[1]
+    }
+    if(r > 1 & r < length(HSI.md)) {
+      dat.class$HSI.md[r] <- mean(bgroundHSI[which(bgroundHSI >= thresholds[(r-1)] &
+                                                     bgroundHSI < thresholds[r])])
+      dat.class$HSI.st[r] <- thresholds[(r-1)]
+      dat.class$HSI.end[r] <- thresholds[r]
+    }
+    if(r == length(HSI.md)) {
+      dat.class$HSI.md[r] <- mean(bgroundHSI[which(bgroundHSI >= thresholds[(r-1)])])
+      dat.class$HSI.st[r] <- thresholds[(r-1)]
+      dat.class$HSI.end[r] <- 1
+    }
+  }
   dat.class$no_nests <- NA
   dat.class$Density <- NA
   for(r in 1:nrow(dat.class)) {
--- R/plotDens.R
+++ R/plotDens.R
@@ -1,26 +1,21 @@
 plotDens <- function(dat.plot, dat.class, sampleHSIs, thresholds, binPntSize = 2, classPntSize = 5,
-                         axisLabSize = 20, tickLabSize = 15, classLabSize = 5, lab.params,
-                         BS = F, ylabel = "Observed density (nests per 100 ha)",
+                     axisLabSize = 20, tickLabSize = 15,
+                     BS = F, ylabel = "Observed density (nests per 100 ha)",
                      xlabel = "Habitat Suitability Index (HSI)") {
   theme_set(theme_bw())
+  if(nrow(dat.class) != (length(thresholds) - 1)) stop("Incorrect number of thresholds provided for the number of suitability classes.")
   plt <- ggplot(dat.plot, aes(HSI.md, Density)) +
     geom_point(size = binPntSize, shape = 16) +
     geom_point(data = dat.class, aes(x = HSI.md, y = Density), size = classPntSize, shape = 1, stroke = 1.5) +
     geom_rug(data = data.frame(HSI = sampleHSIs), aes(x = HSI, y = NULL), colour = "black",
              alpha = 0.3, size = 1) +
-    geom_vline(xintercept = thresholds[1], linetype = "dashed") +
-    geom_vline(xintercept = thresholds[2], linetype = "dashed") +
     ylab(ylabel) + xlab(xlabel) +
     theme(axis.text.x=element_text(size=tickLabSize)) +
     theme(axis.text.y=element_text(size=tickLabSize)) +
     theme(axis.title.x=element_text(size=axisLabSize)) +
-    theme(axis.title.y=element_text(size=axisLabSize)) +
-    annotate("text", x = lab.params[["maxSSS"]][1], y = lab.params[["maxSSS"]][2], label = "maxSSS", angle = 90) +
-    annotate("text", x = lab.params[["Low"]][1], y = lab.params[["Low"]][2], label = "Low", size = classLabSize) +
-    annotate("text", x = lab.params[["Moderate"]][1], y = lab.params[["Moderate"]][2], label = "Moderate",
-             size = classLabSize, angle = lab.params[["Moderate"]][3]) +
-    annotate("text", x = lab.params[["High"]][1], y = lab.params[["High"]][2], label = "High", size = classLabSize)
-  
+    theme(axis.title.y=element_text(size=axisLabSize))
+  for(t in 1:length(thresholds))
+    plt <- plt + geom_vline(xintercept = thresholds[t], linetype = "dashed")
   if(BS == T) plt <- plt +
     geom_errorbar(data = dat.class, aes(x = HSI.md, ymin = Dens95lo, ymax = Dens95hi), size = 1, width = 0.05)
   
