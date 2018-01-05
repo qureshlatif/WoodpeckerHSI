bea7e3ada306f59ff58066c3f1092357ae4dd1b9
Quresh Latif <qureshlatif@yahoo.com>
2018-01-05 13:30:31 -0700

    debug BBWO_MahalHSIs

--- R/BBWO_MahalHSIs.R
+++ R/BBWO_MahalHSIs.R
@@ -5,17 +5,17 @@ BBWO_MahalHSIs <- function(lndscp, nest.samples = BBWO_nest_samples) {
     #BC1 model ################################################################################
     Lnd <- as.matrix(lndscp[,c("DNBR","LOCCC40","LANDCC40")])
     Loc <- as.matrix(Recs[,c("DNBR","LOCCC40","LANDCC40")])
-    M <- Mahal(Loc,Lnd)
+    M <- HSI_Mahal(Loc,Lnd)
     BC1.HSI <- BC1.HSI + round(M,digits=4)
     #TBC1 model ################################################################################
     Lnd<-as.matrix(lndscp[,c("COSASP","DNBR","LOCCC40","LANDCC40")])
     Loc<-as.matrix(Recs[,c("COSASP","DNBR","LOCCC40","LANDCC40")])
-    M<-Mahal(Loc,Lnd)
+    M <- HSI_Mahal(Loc,Lnd)
     TBC1.HSI <- TBC1.HSI + round(M,digits=4)
     #TB1 model ################################################################################
     Lnd<-as.matrix(lndscp[,c("COSASP","DNBR")])
     Loc<-as.matrix(Recs[,c("COSASP","DNBR")])
-    M<-Mahal(Loc,Lnd)
+    M <- HSI_Mahal(Loc,Lnd)
     TB1.HSI <- TB1.HSI + round(M,digits=4)
   }
   #Calculate HSI means and s.e.'s across model replicates
