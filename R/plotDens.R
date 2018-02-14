7e0790009dbcd9eee5ec4da44ea6972f3190306e
Quresh Latif <qureshlatif@yahoo.com>
2018-02-14 14:46:05 -0700

    adjust plotDens
    Put points in front of error bars.

--- R/plotDens.R
+++ R/plotDens.R
@@ -5,8 +5,11 @@ plotDens <- function(dat.plot, dat.class, sampleHSIs, thresholds, binPntSize = 2
   theme_set(theme_bw())
   if(nrow(dat.class) != (length(thresholds) + 1)) stop("Incorrect number of thresholds provided for the number of suitability classes.")
   plt <- ggplot(dat.plot, aes(HSI.md, Density)) +
-    geom_point(size = binPntSize, shape = 16) +
-    geom_point(data = dat.class, aes(x = HSI.md, y = Density), size = classPntSize, shape = 1, stroke = 1.5) +
+    geom_point(size = binPntSize, shape = 16)
+  if(BS == T) plt <- plt +
+    geom_errorbar(data = dat.class, aes(x = HSI.md, ymin = Dens95lo, ymax = Dens95hi), size = 1, width = 0.05)
+  plt <- plt + geom_point(data = dat.class, aes(x = HSI.md, y = Density),
+                          fill = "white", size = classPntSize, shape = 1, stroke = 1.5) +
     geom_rug(data = data.frame(HSI = sampleHSIs), aes(x = HSI, y = NULL), colour = "black",
              alpha = 0.3, size = 1) +
     ylab(ylabel) + xlab(xlabel) +
@@ -16,8 +19,6 @@ plotDens <- function(dat.plot, dat.class, sampleHSIs, thresholds, binPntSize = 2
     theme(axis.title.y=element_text(size=axisLabSize))
   for(t in 1:length(thresholds))
     plt <- plt + geom_vline(xintercept = thresholds[t], linetype = "dashed")
-  if(BS == T) plt <- plt +
-    geom_errorbar(data = dat.class, aes(x = HSI.md, ymin = Dens95lo, ymax = Dens95hi), size = 1, width = 0.05)
   
   return(plt)
 }
